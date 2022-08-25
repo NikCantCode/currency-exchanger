2d58a659317dad7f0b6f4d429656a881e36de97d
NikCantCode <97145858+NikCantCode@users.noreply.github.com>
2022-08-26 01:32:08 +0800

    update

--- currency.js
+++ currency.js
@@ -2,6 +2,8 @@ async function test() {
   const amount = document.getElementById("amountInput").value;
   const fromCurrency = document.getElementById("fromCurrency").value;
   const toCurrency = document.getElementById("toCurrency").value;
+
+  //api query
   const URL =
     "https://api.apilayer.com/exchangerates_data/convert?to=" +
     toCurrency +
@@ -20,14 +22,18 @@ async function test() {
     },
   });
 
+  //fetch from api
   const data = await a.json();
   const { result } = data;
+
+ //display results
   document.getElementById("inputAmount").textContent =
     amount + " " + fromCurrency + " =";
   document.getElementById("outputAmount").textContent =
     result + " " + toCurrency;
 }
 
+//to switch between two select currency
 document.getElementById("exchange").addEventListener("click", () => {
   const from = document.getElementById("fromCurrency").value;
   const to = document.getElementById("toCurrency").value;
